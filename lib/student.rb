2165bcd2f63471187058db9e1b8b81a7f6a1c667
Adam Duff <j.adam.duff@gmail.com>
2018-08-28 22:49:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
   attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  def initialize(name, grade)
+  def initialize(name:, grade:)
     @name = name
     @grade = grade
   end
@@ -37,7 +37,7 @@ class Student
       DB[:conn].execute(sql, self.name, self.grade)
   end
 
-  def self.create(name, grade)
+  def self.create(name:, grade:)
     student = Student.new(name, grade)
     student.save
   end
