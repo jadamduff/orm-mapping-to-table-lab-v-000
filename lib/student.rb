3ae785a7dc19f84ec9c164e44d3868ff5103d931
Adam Duff <j.adam.duff@gmail.com>
2018-08-28 22:28:58 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,10 +14,16 @@ class Student
         id INTEGER PRIMARY KEY,
         name TEXT,
         grade INTEGER
-      )
+      );
       SQL
 
       DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+        DROP TABLE students;
+      SQL
   end
 
 end
